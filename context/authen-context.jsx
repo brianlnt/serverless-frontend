e17452b7a2667be132d442aced01524daa47903c
vnhatle <lenhat.bkit@gmail.com>
2025-04-21 19:03:23 -0500

    Fix error connect to order api, and cognito

--- context/authen-context.jsx
+++ context/authen-context.jsx
@@ -3,13 +3,13 @@
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_5hZ4CSEvQ",
-    client_id: "48foonrut8a0pfog35678mro47",
-    redirect_uri: "http://localhost:3000/",
+    authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL,
     response_type: "code",
-    scope: "email openid phone",
-  };
+    scope: process.env.NEXT_PUBLIC_SCOPES,
+};
 
 export default function ClientOnlyAuthProvider({ children }) {
-  return <AuthProvider {...cognitoAuthConfig}>{children}</AuthProvider>;
+    return <AuthProvider {...cognitoAuthConfig}>{children}</AuthProvider>;
 }
