d94fa3b073411a645cb47b88b8aef371328dc6f1
Brian <thinhle1199@gmail.com>
2025-04-21 20:22:30 -0500

    update scope

--- context/authen-context.jsx
+++ context/authen-context.jsx
@@ -7,7 +7,7 @@ const cognitoAuthConfig = {
     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
     redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL,
     response_type: "code",
-    scope: process.env.NEXT_PUBLIC_SCOPES,
+    scope: "email openid phone https://u7eiihabee.execute-api.us-east-1.amazonaws.com/prod/read https://u7eiihabee.execute-api.us-east-1.amazonaws.com/prod/write",
 };
 
 export default function ClientOnlyAuthProvider({ children }) {
